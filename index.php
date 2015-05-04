bef16f571dbed16179785862ace1162f4637132c
peng <zhipengyel@gmail.com>
2015-05-04 13:16:40 +0800

    new project

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$f3 = require('vendor/fatfree/lib/base.php');
+
+$f3->config('app/config/config.ini');
+$f3->config('app/config/routes.ini');
+
+$f3->set('DEBUG', 2);
+
+// $f3->set('db.dsn', sprintf("%s:host=%s;port=%d;dbname=%s", $f3->get('db.driver'), $f3->get('db.hostname'), $f3->get('db.port'), $f3->get('db.name')));
+// $f3->set('DB', new DB\SQL($f3->get('db.dsn'), $f3->get('db.username'), $f3->get('db.password')));
+
+$f3->set('AUTOLOAD', 'app/controllers/');
+
+$f3->run();
---
+++ tmp/1ckdf3h5iafyq.owtjqd5vv33n.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+<head><title>Test Page</title></head>
+<body>
+  <h1><?php echo $test_var; ?></h1>
+</body>
+</html>
